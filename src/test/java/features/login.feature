3d0a92a94a06c80f4941e74dcd6879a548907949
Felipe Rodrigues <3rd.luis.rodrigues@almundo.com>
2018-10-02 18:21:18 -0300

    add glue in configurations and organize files

--- src/test/java/features/login.feature
+++ src/test/java/features/login.feature
@@ -3,6 +3,6 @@ Feature: Login Feature
 
   Scenario: Login with correct user and password
     Given I navigate to the login page
-    And I enter the username as admin and password as admin
+    And I enter the username as "admin" and password as "adminpassword"
     And I click login button
     Then I should see the userform page
--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,3 +1,5 @@
+package steps;
+
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -6,25 +8,25 @@ import cucumber.api.java.en.Then;
 public class LoginSteps {
     @Given("^I navigate to the login page$")
     public void iNavigateToTheLoginPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
     }
 
-    @And("^I enter the username as admin and password as admin$")
-    public void iEnterTheUsernameAsAdminAndPasswordAsAdmin() throws Throwable {
+    @And("^I enter the username as \"([^\"]*)\" and password as \"([^\"]*)\"$")
+    public void iEnterTheUsernameAsAndPasswordAs(String username, String password) throws Throwable {
         // Write code here that turns the phrase above into concrete actions
         throw new PendingException();
     }
 
     @And("^I click login button$")
     public void iClickLoginButton() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+
     }
 
     @Then("^I should see the userform page$")
     public void iShouldSeeTheUserformPage() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+
     }
+
 }
